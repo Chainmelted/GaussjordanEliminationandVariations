548ce6864426fc79f3532f1f9df087cccb84bd14
Jacob Gaskins <jacobgaskins2@gmail.com>
2025-09-29 21:20:42 -0400

    added create column operation

--- gaussjordan.py
+++ gaussjordan.py
@@ -1,8 +1,18 @@
-﻿rowExample = [1,2,3]
+﻿exampleMatrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+## Matrix Operations
 def row_multiplication(row,multiplier):
     builtRow = []
     for element in row:
         builtRow.append(element*multiplier)
     return builtRow
+def create_column(matrix, column_number):
+    for rows in matrix:
+        column = []
+        column.append(rows[column_number-1])
+        print(column)
 
-print(row_multiplication(rowExample,2))
+
+
+
+print(create_column(exampleMatrix, 3))
