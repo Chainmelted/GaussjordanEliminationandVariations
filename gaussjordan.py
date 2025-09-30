fd440431ebc1cff9e16cdc52500c85d56df02cd8
Jacob Gaskins <jacobgaskins2@gmail.com>
2025-09-29 21:24:35 -0400

    added row division

--- gaussjordan.py
+++ gaussjordan.py
@@ -1,10 +1,16 @@
 ﻿exampleMatrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+exampleRow = [2,4,6]
 
 ## Matrix Operations
 def row_multiplication(row,multiplier):
     builtRow = []
     for element in row:
         builtRow.append(element*multiplier)
+    return builtRow
+def row_division(row,divider):
+    builtRow = []
+    for element in row:
+        builtRow.append(element / divider)
     return builtRow
 def create_column(matrix, column_number):
     for rows in matrix:
@@ -15,4 +21,5 @@ def create_column(matrix, column_number):
 
 
 
-print(create_column(exampleMatrix, 3))
+# print(create_column(exampleMatrix, 3))
+# print(row_division(exampleRow, 2))
