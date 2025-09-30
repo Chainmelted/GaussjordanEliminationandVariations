6b954f4792caf4e591aec54875ece21e816cc996
Jacob Gaskins <jacobgaskins2@gmail.com>
2025-09-29 21:02:41 -0400

    added row multiply operation

---
+++ gaussjordan.py
@@ -0,0 +1,8 @@
+﻿rowExample = [1,2,3]
+def row_multiplication(row,multiplier):
+    builtRow = []
+    for element in row:
+        builtRow.append(element*multiplier)
+    return builtRow
+
+print(row_multiplication(rowExample,2))
